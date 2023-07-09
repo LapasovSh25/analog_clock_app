e563bdb42b13533356d2e7faa83fd558818fb8f5
LapasovSh25 <shahzodlapasov25@gmail.com>
2023-07-09 19:44:50 +0500

    first commit

---
+++ android/app/src/main/kotlin/com/example/clock_analog_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.clock_analog_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/analog_clock_for_flutter.dart
@@ -0,0 +1,3 @@
+library analog_clock_for_flutter;
+
+export "home_page.dart";
